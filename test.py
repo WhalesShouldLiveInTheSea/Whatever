3f52da34bb0dcaefe8205a8c8647c0b51a4c05e4
WhalesShouldLiveInTheSea <65228611+WhalesShouldLiveInTheSea@users.noreply.github.com>
2022-06-28 17:17:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -17,6 +17,6 @@ selenium.common.ElementNotInteractableException
 selenium.common.ElementNotInteractableException
 selenium.common.ElementNotInteractableException
 
-temp=4
+temp=5
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
