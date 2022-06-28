23909c1ad78d8f01d3a1e7176490e28360b69c5a
WhalesShouldLiveInTheSea <65228611+WhalesShouldLiveInTheSea@users.noreply.github.com>
2022-06-28 09:03:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,5 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 selenium.common.ElementNotInteractableException
+selenium.common.ElementNotInteractableException
+selenium.common.ElementNotInteractableException
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
