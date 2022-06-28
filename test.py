3fbdd1d4fbd228b7f45a41c0276a4f3a7aa02d9d
varjoja <varjoja@163.com>
2022-06-28 17:18:24 +0800

    004

--- test.py
+++ test.py
@@ -17,6 +17,6 @@ selenium.common.ElementNotInteractableException
 selenium.common.ElementNotInteractableException
 selenium.common.ElementNotInteractableException
 
-temp=4
+temp=6
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
